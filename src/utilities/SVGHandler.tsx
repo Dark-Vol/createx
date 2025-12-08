2f1d97b627455e9396a42269a065695531b3d326
Dark-Vol <overlord28985@gmail.com>
2025-12-08 14:53:05 +0200

    Update SVGHandler utility

--- src/utilities/SVGHandler.tsx
+++ src/utilities/SVGHandler.tsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from 'react';
 
 interface ISVGHandlerProps {
-  children: JSX.Element;
-  hoverBG?: string;
+  children: JSX.Element
+  hoverBG?: string
 }
 
 const SvgHandler = ({ children, hoverBG }: ISVGHandlerProps) => {
+
   const [hoverColor, setHoverColor] = useState('');
 
   useEffect(() => {
     if (hoverBG === 'primary') {
       setHoverColor('hover:[&_path]:fill-primary');
-    } else if (hoverBG === 'red-300') {
+    }
+    if (hoverBG === 'red-300') {
       setHoverColor('hover:[&_path]:fill-red-300');
-    } else if (hoverBG === 'white') {
+    }
+    if (hoverBG === 'white') {
       setHoverColor('hover:[&_path]:fill-white');
     }
-  }, [hoverBG]);
+  }, [hoverColor]);
 
   return (
-    <div className={`${hoverColor} [&_path]:duration-300 cursor-pointer`}>{children}</div>
+    <div className={`${hoverColor} [&_path]:duration-300 cursor-pointer`}>
+      {children}
+
+    </div>
   );
 };
 
