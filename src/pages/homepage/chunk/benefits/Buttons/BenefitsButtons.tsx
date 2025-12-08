a3b31ccae170ed400e49d71e82c10fbd527c3faf
Dark-Vol <overlord28985@gmail.com>
2025-12-08 14:52:54 +0200

    Refactor BenefitsButtons component

--- src/pages/homepage/chunk/benefits/Buttons/BenefitsButtons.tsx
+++ src/pages/homepage/chunk/benefits/Buttons/BenefitsButtons.tsx
@@ -1,35 +1,29 @@
+import BenefitsButton from '@/pages/homepage/chunk/benefits/Buttons/BenefitsButton.tsx';
 import { useState } from 'react';
 
-import BenefitsButton from '@/pages/homepage/chunk/benefits/Buttons/BenefitsButton';
-
 const BenefitsButtons = () => {
+
   const [activeButtonIndex, setActiveButtonIndex] = useState(0);
 
   const buttonsInfo = [
     {
-      title: 'Experienced Tutors',
+      title: 'Experienced Tutors'
     },
     {
-      title: 'Feedback & Support',
+      title: 'Feedback & Support'
     },
     {
-      title: '24/7 Online Library',
+      title: '24/7 Online Library'
     },
     {
-      title: 'Community',
-    },
-  ];
+      title: 'Community'
+    }
+  ]
 
   return (
-    <div className="grid grid-cols-4 mt-10 mb-15">
+    <div className={'grid grid-cols-4 mt-10 mb-15'}>
       {buttonsInfo.map((button, index) => (
-        <BenefitsButton
-          title={button.title}
-          id={index}
-          key={index}
-          activeButtonIndex={activeButtonIndex}
-          setActiveButtonIndex={setActiveButtonIndex}
-        />
+        <BenefitsButton title={button.title} id={index} key={index} activeButtonIndex={activeButtonIndex} setActiveButtonIndex={setActiveButtonIndex}/>
       ))}
     </div>
   );
