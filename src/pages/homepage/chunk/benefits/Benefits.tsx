911f4c92924e6528674399908fd9763e446295ed
Dark-Vol <overlord28985@gmail.com>
2025-12-08 14:52:52 +0200

    Refactor Benefits component

--- src/pages/homepage/chunk/benefits/Benefits.tsx
+++ src/pages/homepage/chunk/benefits/Benefits.tsx
@@ -1,26 +1,28 @@
 import BenefitsButtons from '@/pages/homepage/chunk/benefits/Buttons/BenefitsButtons';
 
-import BenefitsIllustration from '@public/homepage/benefits/benefits-bg.svg';
+const BenefitsIllustration = '/homepage/benefits/benefits-bg.svg';
+
+import './Benefits.scss';
 
 const Benefits = () => {
   return (
-    <div className="mb-30">
+    <div className="benefits-section">
       <div className="container">
         <div>
-          <h4 className="subtitle text-center">Our benefits</h4>
-          <h2 className="title text-center">That's how we do it</h2>
+          <h4 className="subtitle benefits-header">Our benefits</h4>
+          <h2 className="title benefits-header">That's how we do it</h2>
           <BenefitsButtons />
-          <div className="flex items-center justify-between">
-            <div className="basis-[495px]">
-              <h3 className="mb-6 font-black text-[32px] leading-[1.3]">Only practicing tutors</h3>
-              <p className="text-base leading-[1.6] text-gray-800 text-justify">
+          <div className="benefits-content">
+            <div className="benefits-text">
+              <h3 className="benefits-title">Only practicing tutors</h3>
+              <p className="benefits-description">
                 Urna nisi, arcu cras nunc. Aenean quam est lobortis mi non fames dictum suspendisse. Morbi mauris cras
                 massa ut dolor quis sociis mollis augue. Nunc, sodales tortor sit diam mi amet massa. Fermentum diam
                 diam sociis vestibulum. Nulla nisl accumsan, id dignissim massa ut amet. Amet enim, nisi tempus
                 vehicula.
               </p>
             </div>
-            <img src={BenefitsIllustration} alt="Benefits Illustration" />
+            <img src={BenefitsIllustration} alt="Benefits Illustration" className="benefits-image" />
           </div>
         </div>
       </div>
