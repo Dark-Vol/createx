50fa67e7d89bd80b739599b8a545220a67f89abe
Dark-Vol <overlord28985@gmail.com>
2025-12-08 14:53:04 +0200

    Refactor TeamItems component

--- src/pages/homepage/chunk/team/TeamItems.tsx
+++ src/pages/homepage/chunk/team/TeamItems.tsx
@@ -1,38 +1,38 @@
-import TeamItem from '@/pages/homepage/chunk/team/TeamItem';
-
 import TeamPerson1 from '@public/homepage/team/person-1.png';
 import TeamPerson2 from '@public/homepage/team/person-2.png';
 import TeamPerson3 from '@public/homepage/team/person-3.png';
 import TeamPerson4 from '@public/homepage/team/person-4.png';
+import TeamItem from '@/pages/homepage/chunk/team/TeamItem.tsx';
 
 const TeamItems = () => {
+
   const itemsInfo = [
     {
       personImg: TeamPerson1,
       name: 'Dianne Russell',
-      department: 'Founder and CEO',
+      department: 'Founder and CEO'
     },
     {
       personImg: TeamPerson2,
       name: 'Jerome Bell',
-      department: 'Founder and Program Director',
+      department: 'Founder and Program Director'
     },
     {
       personImg: TeamPerson3,
       name: 'Kristin Watson',
-      department: 'Marketer, Curator of Marketing Course',
+      department: 'Marketer, Curator of Marketing Course'
     },
     {
       personImg: TeamPerson4,
       name: 'Marvin McKinney',
-      department: 'PM, Curator of Management Course',
-    },
-  ];
+      department: 'PM, Curator of Management Course'
+    }
+  ]
 
   return (
-    <div className="flex justify-between">
+    <div className={'flex justify-between'}>
       {itemsInfo.map((person, index) => (
-        <TeamItem person={person} key={index} />
+        <TeamItem person={person} key={index}/>
       ))}
     </div>
   );
