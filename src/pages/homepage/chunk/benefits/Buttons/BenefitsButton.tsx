ea9aff72ae8527bdb8466cc7e4f7b62094bc40ce
Dark-Vol <overlord28985@gmail.com>
2025-12-08 14:52:53 +0200

    Refactor BenefitsButton component

--- src/pages/homepage/chunk/benefits/Buttons/BenefitsButton.tsx
+++ src/pages/homepage/chunk/benefits/Buttons/BenefitsButton.tsx
@@ -1,29 +1,19 @@
+import { BenefitsButtonsTypes } from '@/types/homepage/benefits/benefits-buttons.types.ts';
 import { FC } from 'react';
 import { Layers, MessagesSquare, Star, ThumbsUp } from 'lucide-react';
 
-import { BenefitsButtonsTypes } from '@/types/homepage/benefits/benefits-buttons.types';
-
-const BenefitsButton: FC<BenefitsButtonsTypes> = ({
-  title,
-  id,
-  activeButtonIndex,
-  setActiveButtonIndex,
-}) => {
+const BenefitsButton: FC<BenefitsButtonsTypes> = ({title, id, activeButtonIndex, setActiveButtonIndex}) => {
   const handleClick = () => {
-    if (activeButtonIndex === id) return;
+    if (activeButtonIndex === id) return false;
     setActiveButtonIndex(id);
-  };
-
-  const icons = [<Star key="star" />, <ThumbsUp key="thumbs" />, <Layers key="layers" />, <MessagesSquare key="messages" />];
+  }
 
   return (
-    <button
-      onClick={handleClick}
-      className={`flex gap-2 items-center justify-center border border-solid leading-[1.6] text-base font-bold min-h-[46px] rounded ${
-        activeButtonIndex === id ? 'border-primary text-primary' : 'border-transparent text-gray-400'
-      }`}
-    >
-      {icons[id]}
+    <button onClick={handleClick} className={`flex gap-2 items-center justify-center border border-solid leading-[1.6] text-base font-bold min-h-[46px] rounded ${activeButtonIndex === id ? 'border-primary text-primary' : 'border-transparent text-gray-400'}`}>
+      {id === 0 && <Star/>}
+      {id === 1 && <ThumbsUp/>}
+      {id === 2 && <Layers/>}
+      {id === 3 && <MessagesSquare/>}
       <span>{title}</span>
     </button>
   );
