453a60b96d6df0422db65eb81fd252ad0d7323b0
Dark-Vol <overlord28985@gmail.com>
2025-12-08 14:52:50 +0200

    Refactor Advantages ListItem component

--- src/pages/homepage/chunk/advantages/ListItem.tsx
+++ src/pages/homepage/chunk/advantages/ListItem.tsx
@@ -1,16 +1,12 @@
 import { CircleCheckBig } from 'lucide-react';
 
-interface ListItemProps {
-  title: string;
-}
-
-const ListItem = ({ title }: ListItemProps) => {
+const ListItem = ({title}: {title: string}) => {
   return (
-    <li className="flex">
+    <li className={'flex'}>
       <div>
-        <CircleCheckBig className="text-primary mr-4 mt-1.5" size={16} />
+        <CircleCheckBig className={'text-primary mr-4 mt-1.5'} size={16}/>
       </div>
-      <span className="text-base leading-[1.6] text-gray-800">{title}</span>
+      <span className={'text-base leading-[1.6] text-gray-800'}>{title}</span>
     </li>
   );
 };
