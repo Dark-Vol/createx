5b9f286d74b8dc50720a42433dbb187064350e23
Dark-Vol <overlord28985@gmail.com>
2025-12-08 14:52:55 +0200

    Refactor CoursesItem component

--- src/pages/homepage/chunk/certificate/Certificate.tsx
+++ src/pages/homepage/chunk/certificate/Certificate.tsx
@@ -1,25 +1,27 @@
-import certificateIllustration from '@public/homepage/certificate/certificate-bg.jpg';
-import logoImage1 from '@public/homepage/certificate/certificate-logo-1.png';
-import logoImage2 from '@public/homepage/certificate/certificate-logo-2.png';
-import logoImage3 from '@public/homepage/certificate/certificate-logo-3.png';
+const certificateIllustration = '/homepage/certificate/certificate-bg.jpg';
+const logoImage1 = '/homepage/certificate/certificate-logo-1.png';
+const logoImage2 = '/homepage/certificate/certificate-logo-2.png';
+const logoImage3 = '/homepage/certificate/certificate-logo-3.png';
+
+import './Certificate.scss';
 
 const Certificate = () => {
   return (
-    <section className="py-30">
-      <div className="container relative">
-        <div className="max-w-[397px]">
+    <section className="certificate-section">
+      <div className="container certificate-content">
+        <div className="certificate-text">
           <h4 className="subtitle">Createx Certificate</h4>
           <h2 className="title">Your expertise will be confirmed</h2>
-          <p className="mt-12 mb-8 text-base leading-[1.6] text-gray-800">
+          <p className="certificate-description">
             We are accredited by international professional organizations and institutes:
           </p>
-          <div className="flex gap-10 items-center">
+          <div className="certificate-logos">
             <img src={logoImage1} alt="Certificate Logo" />
             <img src={logoImage2} alt="Certificate Logo" />
             <img src={logoImage3} alt="Certificate Logo" />
           </div>
         </div>
-        <img className="absolute top-0 right-0" src={certificateIllustration} alt="Certificate Illustration" />
+        <img className="certificate-illustration" src={certificateIllustration} alt="Certificate Illustration" />
       </div>
     </section>
   );
