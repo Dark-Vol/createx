cf8b4570726719aebcb0e7f060603c76ffd668a4
Dark-Vol <overlord28985@gmail.com>
2025-12-08 14:52:49 +0200

    Refactor Advantages component

--- src/pages/homepage/chunk/advantages/Advantages.tsx
+++ src/pages/homepage/chunk/advantages/Advantages.tsx
@@ -1,17 +1,19 @@
 import ListItems from '@/pages/homepage/chunk/advantages/ListItems';
 import UILink from '@/UI/link/UILink';
 
-import advantagesIllustration from '@public/homepage/advantages/advantages-bg.png';
+const advantagesIllustration = '/homepage/advantages/advantages-bg.png';
+
+import './Advantages.scss';
 
 const Advantages = () => {
   return (
-    <div className="my-[180px]">
+    <div className="advantages-section">
       <div className="container">
-        <div className="flex justify-between">
-          <div>
+        <div className="advantages-content">
+          <div className="advantages-image">
             <img src={advantagesIllustration} alt="Advantages Illustration" />
           </div>
-          <div className="basis-[390px]">
+          <div className="advantages-text">
             <h4 className="subtitle">Who we are</h4>
             <h2 className="title">Why Createx?</h2>
             <ListItems />
