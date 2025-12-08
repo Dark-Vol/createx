ddee5e6e748f0b6efd4f1c429a069abacaeb4c49
Dark-Vol <overlord28985@gmail.com>
2025-12-08 14:49:39 +0200

    Refactor UIButton component

--- src/UI/form/button/UIButton.tsx
+++ src/UI/form/button/UIButton.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom';
 
-import styles from './UIButton.module.css';
+import './UIButton.scss';
 
 interface UIButtonProps {
   title: string;
@@ -13,17 +13,12 @@ const UIButton = ({ title, isSubmit, isCompasionPasswords }: UIButtonProps) => {
   const showPasswordError = pathname === '/register' && !isCompasionPasswords;
 
   return (
-    <div className="relative">
-      <button
-        type={isSubmit ? 'submit' : 'button'}
-        className={`${styles.button} text-base bg-primary text-white border-primary hover:bg-hover-bg`}
-      >
+    <div className="ui-button-wrapper">
+      <button type={isSubmit ? 'submit' : 'button'} className="ui-button">
         {title}
       </button>
       {showPasswordError && (
-        <p className="absolute -top-6 right-0 leading-4 text-red-600 text-sm select-none">
-          Введённые пароли не совпадают!
-        </p>
+        <p className="ui-button-error">Введённые пароли не совпадают!</p>
       )}
     </div>
   );
